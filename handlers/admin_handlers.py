372ef1025a0db6965e03484584135d3146215235
GeorgeVictor666 <120365126+GeorgeVictor666@users.noreply.github.com>
2024-04-05 12:27:31 +0400

    admin filter

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -3,20 +3,20 @@ from aiogram.filters import Command
 from aiogram.types import CallbackQuery, Message
 from lexicon.lexicon import MESSAGES, INFO
 from database.sessions_admin import admin_upcoming_sessions
-from config_data.config import load_config
 from services.admin import format_sessions
 from filters.admin import IsAdmin
 
 router = Router()
+admin_ids = IsAdmin()
 
 
-@router.message(Command(commands='admin'), IsAdmin())
+@router.message(Command(commands='admin'), admin_ids)
 async def admin_start(message: Message):
     username = message.from_user.username
     await message.answer(INFO['/admin'].format(username), parse_mode='HTML')
 
 
-@router.message(Command(commands='admin_upcoming'), IsAdmin())
+@router.message(Command(commands='admin_upcoming'), admin_ids)
 async def admin_upcoming(message: Message):
     booked_sessions = admin_upcoming_sessions()
     response_message = format_sessions(booked_sessions)
